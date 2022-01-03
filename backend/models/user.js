a4c847294f869acd617233ceb911e88ec8ea33f7
DerzoGit <journetalexandre.contact@gmail.com>
2022-01-03 23:12:42 +0100

    add user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,29 +1,12 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-      models.User.hasMany(models.Post);
-      models.User.hasMany(models.Comment);
-    }
-  };
-  User.init({
-    name: DataTypes.STRING,
-    firstname: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdming: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'User',
-  });
-  return User;
-};
+const { Sequelize, DataTypes } = require("sequelize/dist");
+
+const user = db.define('users', {
+    id: { type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
+    name: { type:DataTypes.STRING(255), allowNull:false },
+    firstname: { type:DataTypes.STRING(255), allowNull:false },
+    email: { type:DataTypes.STRING(255), allowNull:false, unique:true },
+    password: { type:DataTypes.STRING(255), allowNull:false },
+    isAdmin: { type:DataTypes.BOOLEAN, allowNull:false }
+}, {
+    tableName: 'user', timestamp:false, 
+})
