eb7638731886ff8831e857b26b8c682653f084e6
DerzoGit <journetalexandre.contact@gmail.com>
2022-01-17 19:39:26 +0100

    fix error login userId => userId:

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -33,7 +33,7 @@ exports.login = (req, res, next) => {
                 return res.status(401).json({ message: "Mot de passe incorrect "});
             }
             res.status(200).json({ 
-                userId = user.id,
+                userId: user.id,
                 token: jwt.sign({
                     userId: user.id
                 },
