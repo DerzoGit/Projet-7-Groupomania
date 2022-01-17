daf5e806f86905ebf8baa23ce1fe58e5a5d47b3f
DerzoGit <journetalexandre.contact@gmail.com>
2022-01-17 19:52:09 +0100

    change datatypes.string, default255

--- backend/models/user.js
+++ backend/models/user.js
@@ -3,10 +3,10 @@ const sequelize = new Sequelize('sqlite::memory:');
 
 const User = sequelize.define('users', {
     id: { type:DataTypes.INTEGER, autoIncrement:true, primaryKey:true },
-    name: { type:DataTypes.STRING(255), allowNull:false },
-    firstname: { type:DataTypes.STRING(255), allowNull:false },
-    email: { type:DataTypes.STRING(255), allowNull:false, unique:true },
-    password: { type:DataTypes.STRING(255), allowNull:false },
+    name: { type:DataTypes.STRING, allowNull:false },
+    firstname: { type:DataTypes.STRING, allowNull:false },
+    email: { type:DataTypes.STRING, allowNull:false, unique:true },
+    password: { type:DataTypes.STRING, allowNull:false },
     isAdmin: { type:DataTypes.BOOLEAN }
 }, {
     tableName: 'user', timestamp:false
